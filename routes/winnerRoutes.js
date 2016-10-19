a06cdc14efe76533f0dcdc19592713680ad0e95a
Thuong Ho <thuong.t.ho@gmail.com>
2016-10-18 22:15:23 -0700

    basic server and router up and running

---
+++ routes/winnerRoutes.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  var express = require('express');
+
+  var routes = function (Winner) {
+    var winnerRouter = express.Router();
+
+    return winnerRouter;
+  };
+
+  module.exports = routes;
+})
--- server.js
+++ server.js
@@ -5,7 +5,7 @@
       mongoose = require('mongoose'),
       bodyParser = require('body-parser');
 
-  var Winner = require('./models/winnerModel')
+  var Winner = require('./models/winnerModel');
 
   var app = express();
 
@@ -14,9 +14,10 @@
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
 
-  var winnerRouter = require('./routes/winnerRoutes')(Winner);
+  // var winnerRouter = require('./routes/winnerRoutes.js')(Winner);
+  var winnerRouter = require('./routes/winnerRoutes.js');
 
-  // app.use('/api/winners', )
+  // app.use('/api/winners', winnerRouter);
 
   app.get('/', function (req, res) {
     res.send('Cloudy with a chance of retirement.');
