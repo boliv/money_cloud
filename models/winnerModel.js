f32521adbb7bb8cba3a5f12c173ef8e41aa570f5
Thuong Ho <thuong.t.ho@gmail.com>
2016-11-13 17:02:18 -0800

    wip update model

--- models/winnerModel.js
+++ models/winnerModel.js
@@ -7,13 +7,15 @@
   var winnerModel = new Schema({
       lotteryName: {type: String, unique: true, index: true},
       winningPrizes: {type: Object},
-      winningNumbers: {type: Array},
       drawDate: {type: Date, default: Date.now}
+      winningDate: {type: String, unique: true},
+      winningNumbers: {type: Object}
   });
 
   /************* DB ************
   {
     "lotteryName": "Mega",
+<<<<<<< 0b9321e1ec7bb231c2a9c7a1fc320f3d3a8c45c1
     "10-12-16": {
       "winningPrizes": {
             "5 + MEGA": "41,000,000",
@@ -49,6 +51,26 @@
 
       ]
     },
+=======
+    "winningDate": "10-12-16"
+    "winningNumbers": 
+    {
+      ""
+    }
+    "winningCash":
+    {
+      "5 + MEGA": "41,000,000",
+      "5": "43,879",
+      "4 + MEGA": "1,828",
+      "4": "96",
+      "3 + MEGA": "61",
+      "3": "10",
+      "2 + MEGA": "11",
+      "1 + MEGA": "2",
+      "MEGA": "1"
+    }
+  },
+>>>>>>> wip update model
   {
     lotteryName: "Power Ball"
   },
